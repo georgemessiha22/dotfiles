154eb4f0c34ec138bc6c312161707675aa2a7f2d
George Messiha <georgemessiha22@gmail.com>
2024-12-04 11:36:33 +0400

    feat: wezterm

---
+++ dotfiles/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 16
+
+config.color_scheme = "Materia (base16)"
+config.enable_tab_bar = false
+wezterm.max_fps = 120
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- and finally, return the configuration to wezterm
+return config
