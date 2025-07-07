c56640b4063c66bb03d03e40a431cff29d475f36
George Messiha <georgemessiha22@gmail.com>
2025-07-07 12:22:29 +0400

    feat: updating rustup

--- dotfiles/dot_config/fish/conf.d/rustup.fish
+++ dotfiles/dot_config/fish/conf.d/rustup.fish
@@ -1,5 +1,7 @@
+set -gx CARGO_HOME $HOME/.local/cargo
+set -gx RUSTUP_HOME $HOME/.local/rustup
 
 ## Add rust from .local
-if test -e $HOME/.cargo/env.fish
-    source $HOME/.cargo/env.fish
+if test -e $HOME/.local/cargo/env.fish
+    . $HOME/.local/cargo/env.fish
 end
