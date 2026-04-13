18956f1db0324f89e2c0d4d0e1f3cdaa373a479f
George Messiha <georgemessiha22@gmail.com>
2026-04-13 12:27:21 +0400

    feat: adding dotfyle README.md

--- dotfiles/dot_config/nvim/init.lua
+++ dotfiles/dot_config/nvim/init.lua
@@ -1,1 +1,1 @@
-vim.pack.add({ { src = "https://github.com/georgemessiha22/GogoNvim", version = "feat/convert_to_pack", data = { confirm = false } } })
+vim.pack.add({ { src = "https://github.com/georgemessiha22/GogoNvim", version = "master", data = { confirm = false } } })
